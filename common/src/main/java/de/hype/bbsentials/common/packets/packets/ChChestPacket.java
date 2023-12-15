64e125ae35ffc42b454b3c9b2bb78605555c51bd
hackthetime <l4bg0jb7@duck.com>
2023-12-15 13:16:54 +0100

    small change otg with missing push from desktop

--- common/src/main/java/de/hype/bbsentials/common/packets/packets/ChChestPacket.java
+++ common/src/main/java/de/hype/bbsentials/common/packets/packets/ChChestPacket.java
@@ -5,14 +5,16 @@ import de.hype.bbsentials.common.constants.enviromentShared.ChChestItem;
 
 public class ChChestPacket extends AbstractPacket {
     /**
+     * @param lobbyId the id of the lobby
      * @param announcerUsername person who found the chest.
      * @param items             items contained in the chest. {@link de.hype.bbsentials.common.constants.enviromentShared.ChChestItems (available constants)}
      * @param locationCoords    coordinates of the chest.
      * @param bbcommand         command that shall be executed in game to get an invitation to the party.
      * @param extraMessage      a message that is optional to be provided by the announcer for custom additional info
      */
-    public ChChestPacket(String announcerUsername, ChChestItem[] items, String locationCoords, String bbcommand, String extraMessage) {
+    public ChChestPacket(int lobbyId, String announcerUsername, ChChestItem[] items, String locationCoords, String bbcommand, String extraMessage) {
         super(1, 1); //Min and Max supported Version
+        this.lobbyId = lobbyId;
         this.announcerUsername = announcerUsername;
         this.locationCoords = locationCoords;
         this.bbcommand = bbcommand;
@@ -20,10 +22,10 @@ public class ChChestPacket extends AbstractPacket {
         this.items = items;
     }
 
+    public final int lobbyId;
     public final String announcerUsername;
     public final String locationCoords;
     public final String bbcommand;
     public final String extraMessage;
     public final ChChestItem[] items;
-
 }

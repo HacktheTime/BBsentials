7a656e666f0d5e02195e203c2edfac134463d115
hackthetime <l4bg0jb7@duck.com>
2024-01-31 09:03:43 +0100

    bug fixes

--- common/src/main/java/de/hype/bbsentials/client/common/config/PartyConfig.java
+++ common/src/main/java/de/hype/bbsentials/client/common/config/PartyConfig.java
@@ -1,7 +1,12 @@
 package de.hype.bbsentials.client.common.config;
 
+import de.hype.bbsentials.client.common.chat.Chat;
 import de.hype.bbsentials.client.common.client.objects.TrustedPartyMember;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,12 +32,18 @@ public class PartyConfig extends BBsentialsConfig {
     }
 
     public TrustedPartyMember getTrustedUsername(String username) {
+        String uuid = getMcUUIDbyUsername(username);
+        if (uuid == null) {
+            Chat.sendPrivateMessageToSelfError("Invalid Username → uuid could not be retrieved");
+            return null;
+        }
+        Chat.sendPrivateMessageToSelfImportantInfo("Collecting Data. May take some time");
         for (TrustedPartyMember trustedPartyMember : trustedPartyMembers) {
-            if (trustedPartyMember.getUsername().equals(username)) return trustedPartyMember;
+            if (trustedPartyMember.mcUuid.equals(uuid)) return trustedPartyMember;
         }
         if (useRecommendedTrustedMembers) {
             for (TrustedPartyMember recommendedTrustedMember : recommendedTrustedMembers) {
-                if (recommendedTrustedMember.getUsername().equals(username)) return recommendedTrustedMember;
+                if (recommendedTrustedMember.mcUuid.equals(uuid)) return recommendedTrustedMember;
             }
         }
         return null;
@@ -47,5 +58,38 @@ public class PartyConfig extends BBsentialsConfig {
         recommendedTrustedMembers.add(TrustedPartyMember.fromUUID("322eb7ef60a44956a707a22e9630bdb8").partyAdmin(true));//HunterhiHunter
         recommendedTrustedMembers.add(TrustedPartyMember.fromUUID("ca9b4a62a38b481d94653383ba0d371c").partyAdmin(true));//ooffyy
         recommendedTrustedMembers.add(TrustedPartyMember.fromUUID("d36c28e4157f407394c385123d8b42cf"));//pois, because troller no admin perms
+        recommendedTrustedMembers.add(TrustedPartyMember.fromUUID("5a4245b07efa45019f162028ba713bb1").partyAdmin(true));//skyrezz bingo Hypixel admin
+        recommendedTrustedMembers.add(TrustedPartyMember.fromUUID("858ee9a1ba6f4ef9a512fd0eb63d0663").partyAdmin(true));//Arithemonkey
+        recommendedTrustedMembers.add(TrustedPartyMember.fromUUID("c1127c0f10cd489c9d0f27c2df3d76f1").partyAdmin(true));//Godwyn
+
+    }
+
+    private static String getMcUUIDbyUsername(String username) {
+        try {
+            String url = "https://api.mojang.com/users/profiles/minecraft/" + username;
+            URL mojangAPI = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) mojangAPI.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == 200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                reader.close();
+
+                // Parse the JSON response
+                String uuid = response.toString().split("\"")[3];
+                return uuid;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }

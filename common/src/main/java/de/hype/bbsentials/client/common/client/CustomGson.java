57d4144568111c80d2c64031969ba57ddc135362
HacktheTime <l4bg0jb7@duck.com>
2024-02-04 18:17:37 +0100

    crash fix by making a color serializer

--- common/src/main/java/de/hype/bbsentials/client/common/client/CustomGson.java
+++ common/src/main/java/de/hype/bbsentials/client/common/client/CustomGson.java
@@ -1,14 +1,41 @@
 package de.hype.bbsentials.client.common.client;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
+
+import java.awt.*;
+import java.lang.reflect.Type;
 
 public class CustomGson {
     public static Gson create() {
         return new GsonBuilder()
 //                .registerTypeHierarchyAdapter(BBDisplayNameProvider.class, new BBDisplayNameProviderSerializer())
+                .registerTypeAdapter(Color.class, new ColorSerializer())
                 .create();
 
+    }
+
+    private static class ColorSerializer implements JsonSerializer<Color>, JsonDeserializer<Color> {
+        @Override
+        public Color deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            JsonObject jsonObject = json.getAsJsonObject();
+            int red = jsonObject.getAsJsonPrimitive("r").getAsInt();
+            int green = jsonObject.getAsJsonPrimitive("g").getAsInt();
+            int blue = jsonObject.getAsJsonPrimitive("b").getAsInt();
+            int alpha = jsonObject.getAsJsonPrimitive("a").getAsInt();
+
+            return new Color(red, green, blue, alpha);
+        }
+
+        @Override
+        public JsonElement serialize(Color src, Type typeOfSrc, JsonSerializationContext context) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("r", src.getRed());
+            jsonObject.addProperty("g", src.getGreen());
+            jsonObject.addProperty("b", src.getBlue());
+            jsonObject.addProperty("a", src.getAlpha());
+
+            return jsonObject;
+        }
     }
 
 //    private static class BBDisplayNameProviderSerializer implements JsonSerializer<BBDisplayNameProvider>, JsonDeserializer<BBDisplayNameProvider> {

bcb0c0fd2187d5514bee0f35ff97cd2684fb2393
huskestuggu <edvin.rykhus@gmail.com>
2024-02-16 08:32:01 +0100

    MobEnums typo

--- common/src/main/java/de/hype/bbsentials/shared/constants/MobGroupEnums.java
+++ common/src/main/java/de/hype/bbsentials/shared/constants/MobGroupEnums.java
@@ -3,7 +3,7 @@ package de.hype.bbsentials.shared.constants;
 public enum MobGroupEnums {
     Enderman("Enderman", MobEnums.Enderman_42),
     TEAM_TREASURITE("Team Treasurite"),
-    GOBLIN("Team Treasurite"),
+    GOBLIN("GOBLIN"),
     Nether_Bosses("Crimson Isle Minibosses", MobEnums.Ashfang_200, MobEnums.Magma_Boss_500, MobEnums.Mage_Outlaw_200, MobEnums.Barbarian_duke_x_200, MobEnums.Blade_Soul_200),
     Ender_Dragons("Ender Dragons", null);
     private final MobEnums[] mobEnums;

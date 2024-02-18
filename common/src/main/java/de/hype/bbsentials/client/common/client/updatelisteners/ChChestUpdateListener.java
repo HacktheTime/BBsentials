17a7211bf5054f57a2b5a1dc6abf1c5f050b56f4
HacktheTime <l4bg0jb7@duck.com>
2024-02-18 15:17:37 +0100

    bug fixes with chchests

--- common/src/main/java/de/hype/bbsentials/client/common/client/updatelisteners/ChChestUpdateListener.java
+++ common/src/main/java/de/hype/bbsentials/client/common/client/updatelisteners/ChChestUpdateListener.java
@@ -1,5 +1,6 @@
 package de.hype.bbsentials.client.common.client.updatelisteners;
 
+import de.hype.bbsentials.client.common.chat.Chat;
 import de.hype.bbsentials.client.common.client.BBsentials;
 import de.hype.bbsentials.client.common.client.objects.ServerSwitchTask;
 import de.hype.bbsentials.client.common.mclibraries.EnvironmentCore;
@@ -42,11 +43,13 @@ public class ChChestUpdateListener extends UpdateListener {
                 waypoint.visible = shouldDisplay;
                 continue;
             }
-            List<String> chestItems = Arrays.stream(chest.items).filter(item-> !item.isCustom()).map(ChChestItem::getDisplayName).collect(Collectors.toList());
+            List<String> chestItems = Arrays.stream(chest.items).filter(item -> !item.isCustom()).map(ChChestItem::getDisplayName).collect(Collectors.toList());
             List<RenderInformation> renderInformationList = new ArrayList<>();
-            chestItems.forEach((item)->renderInformationList.add(new RenderInformation("bbsentials","textures/gui/sprites/waypoints/"+item+".png")));
-            Waypoints newpoint = new Waypoints(chest.coords, "{\"text\":\"" + String.join(", ", chestItems.subList(0, Math.min(chestItems.size(), 3))) + "\"}", 1000, shouldDisplay, true, renderInformationList,BBsentials.chChestConfig.defaultWaypointColor,BBsentials.chChestConfig.doChestWaypointsTracers);
-            waypoints.put(newpoint.position, newpoint);
+            chestItems.forEach((item) -> renderInformationList.add(new RenderInformation("bbsentials", "textures/gui/sprites/waypoints/" + item + ".png")));
+            if (Waypoints.waypoints.values().stream().noneMatch(waypointFiltered -> waypointFiltered.position.equals(chest.coords))) {
+                Waypoints newpoint = new Waypoints(chest.coords, "{\"text\":\"" + String.join(", ", chestItems.subList(0, Math.min(chestItems.size(), 3))) + "\"}", 1000, shouldDisplay, true, renderInformationList, BBsentials.chChestConfig.defaultWaypointColor, BBsentials.chChestConfig.doChestWaypointsTracers);
+                waypoints.put(newpoint.position, newpoint);
+            }
         }
     }
 
@@ -83,12 +86,21 @@ public class ChChestUpdateListener extends UpdateListener {
     }
 
     public void setStatus(StatusConstants newStatus) {
+        setStatusNoUpdate(newStatus);
+        sendUpdatePacket();
+    }
+
+    public void setStatusNoUpdate(StatusConstants newStatus) {
         if (lobby.getStatus().equals(newStatus.getDisplayName())) return;
         try {
             lobby.setStatus(newStatus);
         } catch (SQLException e) {
             //never thrown lol
         }
+    }
+
+    public void sendUpdatePacket() {
+        updateMetaData();
         getConnection().sendPacket(new ChestLobbyUpdatePacket(lobby));
     }
 
@@ -102,9 +114,21 @@ public class ChChestUpdateListener extends UpdateListener {
     }
 
     public void addOpenedChest(Position pos) {
-        BBsentials.executionService.execute(()->{
+        BBsentials.executionService.execute(() -> {
             chestsOpened.add(pos);
             setWaypoints();
         });
+    }
+
+    public void updateMetaData() {
+        try {
+            long timeTillCloseMili = ((360_000 - EnvironmentCore.utils.getLobbyTime()) * 50);
+            long timeMiliClosingDate = new Date().getTime() + timeTillCloseMili;
+            if (timeTillCloseMili <= 0) setStatusNoUpdate(StatusConstants.CLOSED);
+            lobby.setLobbyMetaData(EnvironmentCore.utils.getPlayers(), timeMiliClosingDate);
+        } catch (SQLException e) {
+            Chat.sendPrivateMessageToSelfError("Uhm how did this happen: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }

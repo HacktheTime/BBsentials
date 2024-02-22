22b5039b749bf7e1a8efddc23d9aef421a736d5f
huskestuggu <edvin.rykhus@gmail.com>
2024-02-22 15:16:02 +0100

    MobEnums

--- common/src/main/java/de/hype/bbsentials/shared/constants/MobEnums.java
+++ common/src/main/java/de/hype/bbsentials/shared/constants/MobEnums.java
@@ -1,15 +1,20 @@
 package de.hype.bbsentials.shared.constants;
 
 public enum MobEnums {
-    Enderman_42("enderman_42", "Enderman", 42, MobGroupEnums.Enderman, Islands.THE_END),
-    Enderman_45("enderman_45", "Enderman", 45, MobGroupEnums.Enderman, Islands.THE_END),
-    Enderman_50("enderman_50", "Enderman", 50, MobGroupEnums.Enderman, Islands.THE_END),
-    Golden_Goblin("goblin_50", "Golden Goblin", 50, MobGroupEnums.GOBLIN, Islands.DWARVEN_MINES, Islands.CRYSTAL_HOLLOWS),
-    Ashfang_200("ashfang_200", "Ashfang", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
-    Barbarian_duke_x_200("barbarian_duke_x_200", "Barbarian Duke X", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
-    Mage_Outlaw_200("mage_outlaw_200", "Mage Outlaw", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
-    Blade_Soul_200("blade_soul_200", "Blade Soul", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
-    Magma_Boss_500("magma_boss_500", "Magma Boss", 500, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
+//    Graveyard_Zombie("graveyard_zombie_1", "Graveyard Zombie", 1, MobGroupEnums.Zombie, Islands.HUB),
+//    Zombie_Villager("zombie_villager_1", "Zombie Villager", 1, MobGroupEnums.Zombie, Islands.HUB)
+//    Crypt_Ghoul("unburried_zombie_30", "Crypt Ghoul", 30, MobGroupEnums.Zombie, Islands.HUB),
+//    Golden_Ghoul("unburried_zombie_60", "Golden Ghoul", 60, MobGroupEnums.Zombie, Islands.HUB),
+//    Wolf("ruin_wolf_15", "Wolf", 15, MobGroupEnums.Wolf, Islands.HUB),
+//    Enderman_42("enderman_42", "Enderman", 42, MobGroupEnums.Enderman, Islands.THE_END),
+//    Enderman_45("enderman_45", "Enderman", 45, MobGroupEnums.Enderman, Islands.THE_END),
+//    Enderman_50("enderman_50", "Enderman", 50, MobGroupEnums.Enderman, Islands.THE_END),
+//    Golden_Goblin("goblin_50", "Golden Goblin", 50, MobGroupEnums.GOBLIN, Islands.DWARVEN_MINES, Islands.CRYSTAL_HOLLOWS),
+//    Ashfang_200("ashfang_200", "Ashfang", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
+//    Barbarian_Duke_x_200("barbarian_duke_x_200", "Barbarian Duke X", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
+//    Mage_Outlaw_200("mage_outlaw_200", "Mage Outlaw", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
+//    Blade_Soul_200("blade_soul_200", "Blade Soul", 200, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
+//    Magma_Boss_500("magma_boss_500", "Magma Boss", 500, MobGroupEnums.Nether_Bosses, Islands.CRIMSON_ISLE),
 
 
     ;
--- common/src/main/java/de/hype/bbsentials/shared/constants/MobGroupEnums.java
+++ common/src/main/java/de/hype/bbsentials/shared/constants/MobGroupEnums.java
@@ -1,11 +1,16 @@
 package de.hype.bbsentials.shared.constants;
 
 public enum MobGroupEnums {
-    Enderman("Enderman", MobEnums.Enderman_42),
-    TEAM_TREASURITE("Team Treasurite"),
-    GOBLIN("Team Treasurite"),
-    Nether_Bosses("Crimson Isle Minibosses", MobEnums.Ashfang_200, MobEnums.Magma_Boss_500, MobEnums.Mage_Outlaw_200, MobEnums.Barbarian_duke_x_200, MobEnums.Blade_Soul_200),
-    Ender_Dragons("Ender Dragons", null);
+//    Zombie("Zombie", MobEnums.Crypt_Ghoul),
+//    Wolf("Wolf", MobEnums.Wolf),
+//    Enderman("Enderman", MobEnums.Enderman_42),
+//    TEAM_TREASURITE("Team Treasurite"),
+//    GOBLIN("Team Treasurite"),
+//    Nether_Bosses("Crimson Isle Minibosses", MobEnums.Ashfang_200, MobEnums.Magma_Boss_500, MobEnums.Mage_Outlaw_200, MobEnums.Barbarian_Duke_x_200, MobEnums.Blade_Soul_200),
+//    Ender_Dragons("Ender Dragons", null),
+
+
+    ;
     private final MobEnums[] mobEnums;
     public final String groupDisplayName;
 

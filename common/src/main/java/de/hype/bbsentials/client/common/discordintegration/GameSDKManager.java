bb0114b2bce04e5e4516648750f4283afdbb4135
hackthetime <l4bg0jb7@duck.com>
2024-02-22 11:36:56 +0100

    fixed developer affecting bug
    fixed textures

--- common/src/main/java/de/hype/bbsentials/client/common/discordintegration/GameSDKManager.java
+++ common/src/main/java/de/hype/bbsentials/client/common/discordintegration/GameSDKManager.java
@@ -162,25 +162,28 @@ public class GameSDKManager extends DiscordEventAdapter {
             if (BBsentials.developerConfig.devMode) {
                 activity.setDetails("Programming this ↑");
                 activity.setState("Developer Mode: Enabled");
-                activity.assets().setSmallText("BBsentials. A mod by @hackthetime");
-                activity.assets().setSmallImage("bingo_hub");
                 activity.assets().setLargeImage("i_am_root_backup_laugh");
                 activity.assets().setLargeText("I am Root (→ Linux for I'm the Admin)");
-                activity.secrets().setSpectateSecret("bb:rpc:join:" + mcUsername);
                 activity.timestamps().setStart(Instant.now());
+                activity.assets().setSmallText("BBsentials. A mod by @hackthetime");
+                activity.assets().setSmallImage("bingo_hub");
             }
             else {
                 Islands island = EnvironmentCore.utils.getCurrentIsland();
                 if (island != null) {
                     activity.setDetails("Playing Hypixel Skyblock");
                     activity.setState(EnvironmentCore.utils.getServerId() + ": " + EnvironmentCore.utils.getCurrentIsland().getDisplayName());
+                    activity.assets().setSmallText("BBsentials. A mod by @hackthetime");
+                    activity.assets().setSmallImage("bingo_hub");
+                    activity.assets().setLargeImage("bingo_card");
                 }
                 else {
                     activity.setDetails("Playing Minecraft");
                     activity.setState("");
+                    activity.assets().setLargeText("BBsentials. A mod by @hackthetime");
+                    activity.assets().setLargeImage("bingo_hub");
                 }
             }
-
             // Setting a start time causes an "elapsed" field to appear
             activity.timestamps().setStart(Instant.ofEpochSecond(0));
 

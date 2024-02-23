08d3f85674693036bd1bbe06d680542c630d4311
Bingohub <bingohub@hotmail.com>
2024-02-23 17:28:12 +0100

    MobEnums

--- common/src/main/java/de/hype/bbsentials/shared/constants/MobGroupEnums.java
+++ common/src/main/java/de/hype/bbsentials/shared/constants/MobGroupEnums.java
@@ -1,14 +1,14 @@
 package de.hype.bbsentials.shared.constants;
 
 public enum MobGroupEnums {
-//    Zombie("Zombie", MobEnums.Crypt_Ghoul),
-//    Wolf("Wolf", MobEnums.Wolf),
-//    Enderman("Enderman", MobEnums.Enderman_42),
-//    TEAM_TREASURITE("Team Treasurite"),
-//    GOBLIN("Team Treasurite"),
-//    Nether_Bosses("Crimson Isle Minibosses", MobEnums.Ashfang_200, MobEnums.Magma_Boss_500, MobEnums.Mage_Outlaw_200, MobEnums.Barbarian_Duke_x_200, MobEnums.Blade_Soul_200),
-//    Ender_Dragons("Ender Dragons", null),
-
+    Zombie("Zombie",MobEnums.Graveyard_Zombie, MobEnums.Zombie_Villager, MobEnums.Crypt_Ghoul, MobEnums.Golden_Ghoul),
+//    Wolf("Wolf", MobEnums.Wolf, MobEnums.Old_Wolf),
+    Enderman("Enderman", MobEnums.Enderman_42, MobEnums.Enderman_45, MobEnums.Enderman_50),
+//    TEAM_TREASURITE("Team Treasurite", MobEnums.Team),
+    GOBLIN("Goblin", MobEnums.Golden_Goblin),
+    Nether_Bosses("Crimson Isle Minibosses", MobEnums.Ashfang_200, MobEnums.Magma_Boss_500, MobEnums.Mage_Outlaw_200, MobEnums.Barbarian_Duke_x_200, MobEnums.Blade_Soul_200),
+    Ender_Dragons("Ender Dragons", null),
+    
 
     ;
     private final MobEnums[] mobEnums;

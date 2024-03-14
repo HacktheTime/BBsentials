314e6f63d4c095038247f0f55a20433a99baa2fb
HacktheTime <l4bg0jb7@duck.com>
2024-03-14 17:19:46 +0100

    added rarity enums

---
+++ common/src/main/java/de/hype/bbsentials/shared/constants/Rarity.java
@@ -0,0 +1,15 @@
+package de.hype.bbsentials.shared.constants;
+
+public enum Rarity {
+    COMMON,
+    UNCOMMON,
+    RARE,
+    EPIC,
+    LEGENDARY,
+    MYTHIC,
+    DIVINE,
+    SPECIAL,
+    VERY_SPECIAL,
+    ULTIMATE,
+    ADMIN
+}

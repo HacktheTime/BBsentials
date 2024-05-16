a13d1c076bbd61cd4f638660255501b8fd22c275
HacktheTime <l4bg0jb7@duck.com>
2024-05-16 21:30:34 +0200

    added missing class and a resource

---
+++ common/src/main/java/de/hype/bbsentials/shared/objects/ComGoalPositionExtendedData.java
@@ -0,0 +1,22 @@
+package de.hype.bbsentials.shared.objects;
+
+import de.hype.bbsentials.shared.packets.function.PositionCommunityFeedback;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+public class ComGoalPositionExtendedData{
+    public final Instant fromTime = Instant.now();
+    public final String mcuuid;
+    public final PositionCommunityFeedback.ComGoalPosition position;
+
+    public ComGoalPositionExtendedData(PositionCommunityFeedback.ComGoalPosition data, String mcuuid) {
+        this.mcuuid = mcuuid;
+        position=data;
+    }
+
+    public Instant getReferenceTime(){
+        int punPos = (100-position.position);
+        return fromTime.plus((long) punPos *punPos, ChronoUnit.MINUTES);
+    }
+}

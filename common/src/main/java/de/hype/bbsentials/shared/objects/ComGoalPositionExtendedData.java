e0386db3bfde05f1568541df16cc723de6235fef
HacktheTime <l4bg0jb7@duck.com>
2024-05-16 23:01:57 +0200

    some bug and reformatting fixes. in regards with gradle setup

--- common/src/main/java/de/hype/bbsentials/shared/objects/ComGoalPositionExtendedData.java
+++ common/src/main/java/de/hype/bbsentials/shared/objects/ComGoalPositionExtendedData.java
@@ -5,18 +5,18 @@ import de.hype.bbsentials.shared.packets.function.PositionCommunityFeedback;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
-public class ComGoalPositionExtendedData{
+public class ComGoalPositionExtendedData {
     public final Instant fromTime = Instant.now();
     public final String mcuuid;
     public final PositionCommunityFeedback.ComGoalPosition position;
 
     public ComGoalPositionExtendedData(PositionCommunityFeedback.ComGoalPosition data, String mcuuid) {
         this.mcuuid = mcuuid;
-        position=data;
+        position = data;
     }
 
-    public Instant getReferenceTime(){
-        int punPos = (100-position.position);
-        return fromTime.plus((long) punPos *punPos, ChronoUnit.MINUTES);
+    public Instant getReferenceTime() {
+        int punPos = (100 - position.position);
+        return fromTime.plus((long) punPos * punPos, ChronoUnit.MINUTES);
     }
 }

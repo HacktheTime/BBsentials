3a0a73f686ca9367036fe74c8aced3f869802028
HacktheTime <l4bg0jb7@duck.com>
2024-05-18 22:10:04 +0200

    fixed waypoints not being rendered

--- fabric/src/main/java/de/hype/bbsentials/fabric/MCEvents.java
+++ fabric/src/main/java/de/hype/bbsentials/fabric/MCEvents.java
@@ -1,20 +1,28 @@
 package de.hype.bbsentials.fabric;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import de.hype.bbsentials.client.common.chat.Chat;
 import de.hype.bbsentials.client.common.chat.Message;
 import de.hype.bbsentials.client.common.client.BBsentials;
 import de.hype.bbsentials.client.common.client.updatelisteners.UpdateListenerManager;
 import de.hype.bbsentials.client.common.config.constants.ClickableArmorStand;
 import de.hype.bbsentials.client.common.mclibraries.EnvironmentCore;
+import de.hype.bbsentials.client.common.objects.Waypoints;
 import de.hype.bbsentials.fabric.mixins.mixinaccessinterfaces.IChestBlockEntityMixinAccess;
 import de.hype.bbsentials.shared.constants.ChChestItem;
 import de.hype.bbsentials.shared.constants.Islands;
 import de.hype.bbsentials.shared.objects.Position;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 import net.minecraft.block.ChestBlock;
 import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.entity.Entity;
@@ -23,12 +31,17 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix4f;
+import org.lwjgl.opengl.GL11;
 
 import java.util.Objects;
 import java.util.Set;
@@ -38,51 +51,51 @@ import java.util.stream.Collectors;
 public class MCEvents implements de.hype.bbsentials.client.common.mclibraries.MCEvents {
     public Utils utils = (Utils) EnvironmentCore.utils;
 
-//    public static void renderWaypoints(WorldRenderContext context) {
-//        Camera camera = context.camera();
-//
-//        MatrixStack matrixStack = new MatrixStack();
-//
-//        for (Waypoints waypoint : Waypoints.waypoints.values()) {
-//            if (waypoint.visible) {
-//                Vec3d waypointPosition = new Vec3d(waypoint.position.x, waypoint.position.y, waypoint.position.z);
-//                double distance = camera.getPos().distanceTo(waypointPosition);
-//                Vec3d transformedPosition = waypointPosition.subtract(camera.getPos());
-//
-//                matrixStack.push();
-//                matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(camera.getPitch()));
-//                matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(camera.getYaw() + 180.0F));
-//                matrixStack.translate(transformedPosition.x, transformedPosition.y, transformedPosition.z);
-//
-//                Matrix4f positionMatrix = matrixStack.peek().getPositionMatrix();
-//                Tessellator tessellator = Tessellator.getInstance();
-//                BufferBuilder buffer = tessellator.getBuffer();
-//
-//                float size = (float) distance / 10;
-//                buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE);
-//                buffer.vertex(positionMatrix, -size, size, 0).color(1f, 1f, 1f, 1f).texture(0f, 0f).next();
-//                buffer.vertex(positionMatrix, -size, -size, 0).color(1f, 0f, 0f, 1f).texture(0f, 1f).next();
-//                buffer.vertex(positionMatrix, size, -size, 0).color(0f, 1f, 0f, 1f).texture(1f, 1f).next();
-//                buffer.vertex(positionMatrix, size, size, 0).color(0f, 0f, 1f, 1f).texture(1f, 0f).next();
-//
-//                RenderSystem.setShader(GameRenderer::getPositionColorTexProgram);
-//                RenderSystem.setShaderTexture(0, new Identifier("bbsentials", "textures/item/prehistoric_egg.png"));
-//                RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-//                RenderSystem.disableCull();
-//                RenderSystem.depthFunc(GL11.GL_ALWAYS);
-//
-//                tessellator.draw();
-//
-//                RenderSystem.depthFunc(GL11.GL_LEQUAL);
-//                RenderSystem.enableCull();
-//
-//                matrixStack.pop();
-//
-////                String distanceText = String.format("%.2f blocks", distance);
-////                MinecraftClient.getInstance().textRenderer.draw(distanceText, 0, 0, 0xFFFFFF);
-//            }
-//        }
-//    }
+    public static void renderWaypoints(WorldRenderContext context) {
+        Camera camera = context.camera();
+
+        MatrixStack matrixStack = new MatrixStack();
+
+        for (Waypoints waypoint : Waypoints.waypoints.values()) {
+            if (waypoint.visible) {
+                Vec3d waypointPosition = new Vec3d(waypoint.position.x, waypoint.position.y, waypoint.position.z);
+                double distance = camera.getPos().distanceTo(waypointPosition);
+                Vec3d transformedPosition = waypointPosition.subtract(camera.getPos());
+
+                matrixStack.push();
+                matrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(camera.getPitch()));
+                matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(camera.getYaw() + 180.0F));
+                matrixStack.translate(transformedPosition.x, transformedPosition.y, transformedPosition.z);
+
+                Matrix4f positionMatrix = matrixStack.peek().getPositionMatrix();
+                Tessellator tessellator = Tessellator.getInstance();
+                BufferBuilder buffer = tessellator.getBuffer();
+
+                float size = (float) distance / 10;
+                buffer.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR_TEXTURE);
+                buffer.vertex(positionMatrix, -size, size, 0).color(1f, 1f, 1f, 1f).texture(0f, 0f).next();
+                buffer.vertex(positionMatrix, -size, -size, 0).color(1f, 0f, 0f, 1f).texture(0f, 1f).next();
+                buffer.vertex(positionMatrix, size, -size, 0).color(0f, 1f, 0f, 1f).texture(1f, 1f).next();
+                buffer.vertex(positionMatrix, size, size, 0).color(0f, 0f, 1f, 1f).texture(1f, 0f).next();
+
+                RenderSystem.setShader(GameRenderer::getPositionColorTexProgram);
+                RenderSystem.setShaderTexture(0, new Identifier("bbsentials", "textures/item/prehistoric_egg.png"));
+                RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+                RenderSystem.disableCull();
+                RenderSystem.depthFunc(GL11.GL_ALWAYS);
+
+                tessellator.draw();
+
+                RenderSystem.depthFunc(GL11.GL_LEQUAL);
+                RenderSystem.enableCull();
+
+                matrixStack.pop();
+
+                String distanceText = String.format("%.2f blocks", distance);
+                MinecraftClient.getInstance().textRenderer.draw(distanceText, 0F, 0F, 0xFFFFFF,false,matrixStack.peek().getPositionMatrix(), context.consumers(), TextRenderer.TextLayerType.SEE_THROUGH,0,LightmapTextureManager.MAX_LIGHT_COORDINATE);
+            }
+        }
+    }
 
     public void registerOverlays() {
         utils = (Utils) EnvironmentCore.utils;
@@ -163,7 +176,7 @@ public class MCEvents implements de.hype.bbsentials.client.common.mclibraries.MC
 
     @Override
     public void registerWaypoints() {
-//        WorldRenderEvents.END.register(MCEvents::renderWaypoints);
+        WorldRenderEvents.END.register(MCEvents::renderWaypoints);
     }
 
 }

97da06bc06ae9eedd216dce046a9c6736f96dd34
hackthetime <l4bg0jb7@duck.com>
2024-07-03 09:09:52 +0200

    cleaned up waypoint constructor code to use less duplicates. no function changes.

--- common/src/main/java/de/hype/bbsentials/client/common/objects/Waypoints.java
+++ common/src/main/java/de/hype/bbsentials/client/common/objects/Waypoints.java
@@ -20,12 +20,7 @@ public class Waypoints extends ClientWaypointData {
 
 
     public Waypoints(Position pos, String jsonTextToRender, int renderDistance, boolean visible, boolean deleteOnServerSwap, RenderInformation render, Color color, boolean doTracer) {
-        super(pos, jsonTextToRender, renderDistance, visible, deleteOnServerSwap, render, color, doTracer);
-        ServerSwitchTask.onServerLeaveTask(() -> {
-            if (this.deleteOnServerSwap)
-                this.removeFromPool();
-        });
-        waypoints.put(waypointId, this);
+        this(pos, jsonTextToRender, renderDistance, visible, deleteOnServerSwap, List.of(render), color, doTracer);
     }
 
     public Waypoints(Position pos, String jsonTextToRender, int renderDistance, boolean visible, boolean deleteOnServerSwap, RenderInformation render) {
@@ -56,12 +51,7 @@ public class Waypoints extends ClientWaypointData {
     }
 
     public Waypoints(WaypointData data) {
-        super(data.position, data.jsonToRenderText, data.renderDistance, data.visible, data.deleteOnServerSwap, data.render, data.color, data.doTracer);
-        ServerSwitchTask.onServerLeaveTask(() -> {
-            if (this.deleteOnServerSwap)
-                this.removeFromPool();
-        });
-        waypoints.put(waypointId, this);
+        this(data.position, data.jsonToRenderText, data.renderDistance, data.visible, data.deleteOnServerSwap, data.render, data.color, data.doTracer);
     }
 
     public Waypoints removeFromPool() {
--- common/src/main/java/de/hype/bbsentials/shared/objects/WaypointData.java
+++ common/src/main/java/de/hype/bbsentials/shared/objects/WaypointData.java
@@ -1,7 +1,6 @@
 package de.hype.bbsentials.shared.objects;
 
 import java.awt.*;
-import java.util.Arrays;
 import java.util.List;
 
 public class WaypointData {
@@ -15,50 +14,19 @@ public class WaypointData {
     public boolean doTracer = true;
 
     public WaypointData(Position pos, String jsonTextToRender, int renderDistance, boolean visible, boolean deleteOnServerSwap, RenderInformation render, boolean doTracer) {
-        this.position = pos;
-        this.jsonToRenderText = jsonTextToRender;
-        this.renderDistance = renderDistance;
-        this.deleteOnServerSwap = deleteOnServerSwap;
-        this.render = Arrays.asList(render);
-        this.visible = visible;
-        this.doTracer = doTracer;
-        this.color = new Color(1f, 1f, 1f);
-        if (jsonTextToRender == null || jsonTextToRender.isEmpty()) {
-            this.jsonToRenderText = "{\"text\":\"Unnamed\"}";
-        }
+        this(pos, jsonTextToRender, renderDistance, visible, deleteOnServerSwap, List.of(render), new Color(1f, 1f, 1f), doTracer);
     }
 
     public WaypointData(Position pos, String jsonTextToRender, int renderDistance, boolean visible, boolean deleteOnServerSwap, RenderInformation render, Color color, boolean doTracer) {
-        this.position = pos;
-        this.jsonToRenderText = jsonTextToRender;
-        this.renderDistance = renderDistance;
-        this.deleteOnServerSwap = deleteOnServerSwap;
-        this.visible = visible;
-        this.render = Arrays.asList(render);
-        this.color = color;
-        this.doTracer = doTracer;
-        if (jsonTextToRender == null || jsonTextToRender.isEmpty()) {
-            this.jsonToRenderText = "{\"text\":\"Unnamed\"}";
-        }
+        this(pos, jsonTextToRender, renderDistance, visible, deleteOnServerSwap, List.of(render), color, doTracer);
     }
 
     public WaypointData(Position pos, String jsonTextToRender, int renderDistance, boolean visible, boolean deleteOnServerSwap, List<RenderInformation> render, boolean doTracer) {
-        this.position = pos;
-        this.jsonToRenderText = jsonTextToRender;
-        this.renderDistance = renderDistance;
-        this.deleteOnServerSwap = deleteOnServerSwap;
-        this.render = render;
-        this.visible = visible;
-        this.doTracer = doTracer;
-        this.color = new Color(1f, 1f, 1f);
-        if (jsonTextToRender == null || jsonTextToRender.isEmpty()) {
-            this.jsonToRenderText = "{\"text\":\"Unnamed\"}";
-        }
+        this(pos, jsonTextToRender, renderDistance, visible, deleteOnServerSwap, render, new Color(1f, 1f, 1f), doTracer);
     }
 
     public WaypointData(Position pos, String jsonTextToRender, int renderDistance, boolean visible, boolean deleteOnServerSwap, List<RenderInformation> render, Color color, boolean doTracer) {
         this.position = pos;
-        this.jsonToRenderText = jsonTextToRender;
         this.renderDistance = renderDistance;
         this.deleteOnServerSwap = deleteOnServerSwap;
         this.visible = visible;
@@ -67,6 +35,9 @@ public class WaypointData {
         this.doTracer = doTracer;
         if (jsonTextToRender == null || jsonTextToRender.isEmpty()) {
             this.jsonToRenderText = "{\"text\":\"Unnamed\"}";
+        }
+        else {
+            this.jsonToRenderText = jsonTextToRender;
         }
     }
 }

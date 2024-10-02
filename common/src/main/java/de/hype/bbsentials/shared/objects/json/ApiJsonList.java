e4a24c9ffea96d0d7ec1db6fa1b4acf5460a9128
HacktheTime <l4bg0jb7@duck.com>
2024-10-02 07:36:47 +0200

    added missing api classes to the github. forgot to git add them originally.

---
+++ common/src/main/java/de/hype/bbsentials/shared/objects/json/ApiJsonList.java
@@ -0,0 +1,26 @@
+package de.hype.bbsentials.shared.objects.json;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ApiJsonList {
+    ApiJson parent;
+    List<ApiJsonElement> values = new ArrayList<>();
+
+    public ApiJsonList(ApiJson parent, String key) {
+        this.parent = parent;
+        JsonElement temp = parent.getJSONElementSafe();
+        if (temp == null) return;
+        JsonArray array = temp.getAsJsonObject().getAsJsonArray(key);
+        for (JsonElement jsonElement : array) {
+            values.add(new ApiJsonElement(jsonElement));
+        }
+    }
+
+    public List<ApiJsonElement> toList() {
+        return new ArrayList<>(values);
+    }
+}

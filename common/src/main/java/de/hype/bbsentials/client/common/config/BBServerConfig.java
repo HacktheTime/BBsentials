54cac3f889d28ab6cbee0ee771ba53bcbb2047de
HacktheTime <l4bg0jb7@duck.com>
2024-10-08 17:24:08 +0200

    changed website over to hackthetime.de
    removed an unused and outdated method

--- common/src/main/java/de/hype/bbsentials/client/common/config/BBServerConfig.java
+++ common/src/main/java/de/hype/bbsentials/client/common/config/BBServerConfig.java
@@ -5,7 +5,7 @@ public class BBServerConfig extends BBsentialsConfig {
     public boolean overrideBingoTime = false;
     public boolean connectToBeta = false;
     public boolean useMojangAuth = true;
-    public String bbServerURL = "static.88-198-149-240.clients.your-server.de";
+    public String bbServerURL = "hackthetime.de";
     public String apiKey = "";
 
     public BBServerConfig() {
--- fabric/src/main/java/de/hype/bbsentials/fabric/screens/BBsentialsConfigScreenFactory.java
+++ fabric/src/main/java/de/hype/bbsentials/fabric/screens/BBsentialsConfigScreenFactory.java
@@ -64,7 +64,7 @@ public class BBsentialsConfigScreenFactory {
         {
             if (BBsentials.generalConfig.getUsername().equalsIgnoreCase("Hype_the_Time")) {
                 server.addEntry(entryBuilder.startTextField(Text.of("Server URL"), BBsentials.bbServerConfig.bbServerURL.replaceAll(".", "*"))
-                        .setDefaultValue("static.88-198-149-240.clients.your-server.de")
+                        .setDefaultValue("hackthetime.de")
                         .setTooltip(Text.of("Place the Server URL of the BBsentials Server here"))
                         .setSaveConsumer((newValue) -> {
                             if (newValue.replace("*", "").trim().isEmpty()) {
@@ -78,7 +78,7 @@ public class BBsentialsConfigScreenFactory {
             }
             else {
                 server.addEntry(entryBuilder.startTextField(Text.of("Server URL"), BBsentials.bbServerConfig.bbServerURL)
-                        .setDefaultValue("static.88-198-149-240.clients.your-server.de")
+                        .setDefaultValue("hackthetime.de")
                         .setTooltip(Text.of("Place the Server URL of the BBsentials Server here"))
                         .setSaveConsumer(newValue -> BBsentials.bbServerConfig.bbServerURL = newValue)
                         .build());

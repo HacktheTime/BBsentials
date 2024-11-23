b4f583725ae991fa6c2ee81b35b87ace565f4c4b
HacktheTime <l4bg0jb7@duck.com>
2024-11-23 18:43:22 +0100

    fixed a mod runtime issue.

--- fabric/src/main/java/de/hype/bbsentials/fabric/mixins/errorspampatches/HypixelLoggerSpamPatcherClientPlayNetworkListener.java
+++ fabric/src/main/java/de/hype/bbsentials/fabric/mixins/errorspampatches/HypixelLoggerSpamPatcherClientPlayNetworkListener.java
@@ -15,21 +15,4 @@ import java.util.List;
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class HypixelLoggerSpamPatcherClientPlayNetworkListener {
 
-    @Shadow
-    protected abstract ParseResults<CommandSource> parse(String command);
-
-    @Redirect(method = "onEntityPassengersSet", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;)V"), remap = false)
-    public void BBsentials$passengerEntityUnknownPatch(Logger instance, String s) {
-        //Do exactly nothing
-    }
-
-    @Redirect(method = "onEntityTrackerUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/data/DataTracker;writeUpdatedEntries(Ljava/util/List;)V"), remap = false)
-    public void BBsentials$EntityUpdateCrashPatcher(DataTracker instance, List<DataTracker.SerializedEntry<?>> entries) {
-        try {
-            instance.writeUpdatedEntries(entries);
-        } catch (Exception e) {
-            System.out.println("Normally this would probably caused a Crash or force disconnect. BBsentials caught that error");
-            e.printStackTrace();
-        }
-    }
 }

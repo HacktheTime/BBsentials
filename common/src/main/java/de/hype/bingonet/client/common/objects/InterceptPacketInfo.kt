fd9ece37665d75f3be76d5b7aa6b62eef09529e8
HacktheTime <l4bg0jb7@duck.com>
2025-03-29 20:13:17 +0100

    cleaned up code

--- common/src/main/java/de/hype/bingonet/client/common/objects/InterceptPacketInfo.kt
+++ common/src/main/java/de/hype/bingonet/client/common/objects/InterceptPacketInfo.kt
@@ -7,16 +7,16 @@ internal fun interface PacketRunnable<T : AbstractPacket?> {
 }
 
 abstract class InterceptPacketInfo<T : AbstractPacket?>(
-    public val clazz: Class<T>,
-    public val cancelPacket: Boolean,
-    public val blockIntercepts: Boolean,
-    public val ignoreIfIntercepted: Boolean,
+    val clazz: Class<T>,
+    val cancelPacket: Boolean,
+    val blockIntercepts: Boolean,
+    val ignoreIfIntercepted: Boolean,
     /**
      * block execution for completion is used whether the checks shall continue or the connection shall be paused until this is completed.
      */
-    public val blockExecutionForCompletion: Boolean
+    val blockExecutionForCompletion: Boolean
 ) : PacketRunnable<T> {
-    public val replyId: Long = -1
+    val replyId: Long = -1
 
     fun matches(packetClass: Class<T>): Boolean {
         return packetClass == clazz

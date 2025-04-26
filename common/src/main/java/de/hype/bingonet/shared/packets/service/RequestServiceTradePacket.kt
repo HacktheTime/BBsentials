9ad3772fe5127d4bcac8a34e4ab3be5a2374236b
HacktheTime <l4bg0jb7@duck.com>
2025-04-26 10:22:07 +0200

    more git moves

--- common/src/main/java/de/hype/bingonet/shared/packets/service/RequestServiceTradePacket.kt
+++
@@ -1,6 +0,0 @@
-package de.hype.bingonet.shared.packets.service
-
-import de.hype.bingonet.environment.packetconfig.AbstractPacket
-import de.hype.bingonet.shared.constants.TradeType
-
-class RequestServiceTradePacket(@JvmField val tradeType: TradeType?) : AbstractPacket(1, 1)
